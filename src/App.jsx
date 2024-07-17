27021ca8ed6af113354118ee8e265a02b66787d5
arijit@2004 <arijitmondal0609@gmail.com>
2024-07-17 23:19:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+import Grid from './components/Grid/Grid'
+import Card from './components/Card/Card'
+
+function App() {
+  
+  return (
+    <>
+      <Grid numberOfcards={9}/>
+
+    </>
+  )
+}
+
+export default App
